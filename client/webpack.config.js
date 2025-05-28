a3d710f0e5ca921f08ce0414e05a1be2818816d3
Ihor Kupratyi <ihor.k@saasjet.com>
2025-05-29 01:25:18 +0300

    ci(v.1.1.1): update packages & webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-require('dotenv').config({ path: '../server/.env' });
-
 module.exports = (env, arguments) => {
     return {
         entry: {
